3a3c8e100817d189d0d2bc642eb08e18bd67239e
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-21 17:35:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+let colorBox = document.querySelectorAll(".square")
+
+for(let i of colorBox){
+    i.addEventListener("mouseenter",changeColorIn)
+    i.addEventListener("mouseleave",changeColorOut)
+
+}
+function changeColorIn(e){
+    for(let i of colorBox){
+        if(i.id != e.target.id){
+            i.style.cssText=`
+			width: 50px;
+			height: 50px;
+			display: inline-block;
+			margin: 10px;
+			background-color: #6F4E37; /*coffee */
+			cursor: pointer;`
+        }
+    }
+}
+function changeColorOut(e){
+    for(let i of colorBox){
+        if(i.id != e.target.id){
+            i.style.cssText=`
+			width: 50px;
+			height: 50px;
+			display: inline-block;
+			margin: 10px;
+			background-color: #E6E6FA; /*lavender */
+			cursor: pointer;`
+        }
+    }
+}
