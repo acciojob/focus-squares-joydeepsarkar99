0b21a0118a0e5c22d83d8136ef8eeb9a774e3a04
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-21 17:49:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,26 +9,14 @@ for(let i of colorBox){
 function changeColorIn(e){
     for(let i of colorBox){
         if(i.id != e.target.id){
-            i.style.cssText=`
-			width: 50px;
-			height: 50px;
-			display: inline-block;
-			margin: 10px;
-			background-color: #6F4E37; /*coffee */
-			cursor: pointer;`
+            i.style.backgroundColor = "#6F4E37";
         }
     }
 }
 function changeColorOut(e){
     for(let i of colorBox){
         if(i.id != e.target.id){
-            i.style.cssText=`
-			width: 50px;
-			height: 50px;
-			display: inline-block;
-			margin: 10px;
-			background-color: #E6E6FA; /*lavender */
-			cursor: pointer;`
+            i.style.backgroundColor = "#E6E6FA";
         }
     }
 }
